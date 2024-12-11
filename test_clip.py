c69b93c07dcbf427f2a722c2001b200c44537697
root <root@ts-8b1d80f5938fcb4701939050874b011c-launcher.ts-8b1d80f5938fcb4701939050874b011c-launcher.qingyuan-1-ailab-mt-qy-qingyuan-1.svc.cluster.local>
2024-12-12 01:06:05 +0800

    fix vocab error

--- test_clip.py
+++ test_clip.py
@@ -4,13 +4,17 @@ from PIL import Image
 import open_clip
 from tqdm import tqdm
 
-file_path = "/apdcephfs_qy3/share_301812049/xiaoyuanliu/data/RAVEN/test_with_held_out_formatted.csv"
+file_path = "/apdcephfs_qy3/share_301812049/xiaoyuanliu/data/RAVEN/test_held_out.csv"
 df = pd.read_csv(file_path)
 
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-model_path = "/apdcephfs_qy3/share_301812049/xiaoyuanliu/Github/open_clip/src/logs/2024_12_08-23_44_12-model_ViT-L-14-lr_5e-06-b_64-j_4-p_amp_bf16/checkpoints/epoch_32.pt"
-model_name = "ViT-L-14"
+model_path = "/apdcephfs_qy3/share_301812049/xiaoyuanliu/Github/open_clip/src/logs/2024_12_09-21_38_23-model_ViT-L-14-336-lr_5e-06-b_32-j_4-p_amp/checkpoints/epoch_32.pt"
+# model_path = '/apdcephfs_qy3/share_301812049/xiaoyuanliu/Github/clip_raven/ViT-L-14-336px.pt'
+# model_path = '/apdcephfs_qy3/share_301812049/xiaoyuanliu/Github/clip_raven/ViT-L-14.pt'
+
+model_name = "ViT-L-14-336"
+# model_name = "ViT-L-14"
 
 model, _, preprocess = open_clip.create_model_and_transforms(model_name = model_name, pretrained = model_path)
 tokenizer = open_clip.get_tokenizer(model_name)
@@ -18,11 +22,11 @@ model.to(device)
 
 correct_predictions = 0
 total_predictions = 0
-for i in tqdm(range(100)):
+for i in tqdm(range(len(df))):
     img_path = df.iloc[i]["filepath"]
     ans = df.iloc[i]["caption"]
     image = preprocess(Image.open(img_path)).unsqueeze(0).cuda(device=device)
-    options = ["1", "2", "3", "4", "5", "6"]
+    options = ["A", "B", "C", "D", "E", "F", "G", "H"]
     text = tokenizer(options).cuda(device=device)
 
     with torch.no_grad(), torch.amp.autocast('cuda'):
